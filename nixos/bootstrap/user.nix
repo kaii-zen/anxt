cfea9af0e12b8ab7a3c4c7d24f30d9801990a156
Shay Bergmann <shaybergmann@gmail.com>
2018-09-16 18:32:43 -0400

    Typo

--- nixos/bootstrap/user.nix
+++ nixos/bootstrap/user.nix
@@ -1,6 +1,6 @@
 # This is supposed to be copied to (actual) /etc/nixos/user.nix
 # by the first SSM agent run, prior to updating channels and
-# and running nixos-rebuild. This essentially allows the bootstrap
+# running nixos-rebuild. This essentially allows the bootstrap
 # run to run on its own before we load the user's configuration.
 # That is to work around an issue where the initial environemnt doesn't
 # have git or tar installed and therefore cannot use any of the
